505934671d294b8f60d4c966499b073f637dcf99
juf468 <julia.franchi7@gmail.com>
2024-06-27 14:05:55 -0300

    default params with es6

---
+++ src/es6/03-default-params.js
@@ -0,0 +1,17 @@
+function newUser(name, age, country){
+    var name = name || 'Oscar';
+    var age = age || 32;
+    var country = country || 'MX';
+    console.log('name, age, country :>> ', name, age, country);
+    //1er clo = oscar 32 mx y 2do clo = david 15 co
+}
+ newUser()
+ newUser('David', 15, 'CO');
+
+// CON ES6 =>
+    function newAdmin(name = 'Oscar', age = 32, country = 'CL'){
+        console.log('name, age, country :>> ', name, age, country);
+         //1er clo = oscar 32 cl y 2do clo = Ana 28 PE
+        }
+    newAdmin();
+    newAdmin('Ana', 28, 'PE');
